56bf48102bc1c8f70821d87fa76532ba02a1bc53
Pilar Giannelli <pilargiannelli@gmail.com>
2026-04-17 20:04:05 -0300

    Ejercicio 1 de Js realizado

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,12 @@
+const nombre = "Pilar";
+const edad = 21;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador++;
+contador++;
+contador++;
+
+console.log("Valor final = ", contador);
