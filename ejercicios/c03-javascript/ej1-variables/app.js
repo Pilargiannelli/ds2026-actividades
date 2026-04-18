0bd57931f5ba322aa8f945001cc44092f5a43542
Pilar Giannelli <pilargiannelli@gmail.com>
2026-04-18 18:31:28 -0300

    Ligero cambio en app.js del ejercicio 1

--- ejercicios/c03-javascript/ej1-variables/app.js
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -11,30 +11,4 @@ contador++;
 
 console.log("Valor final = ", contador);
 
-function clasificarNota(nota){
-    if(nota < 4){
-        return "Desaprobado";
-    }
-    if(nota >= 4 && nota <= 7){
-        return "Aprobado";
-    }
-    if(nota >=8 && nota <= 10){
-        return "Promocionado";
-    }
-    else {
-        return "Nota inválida";
-    }
-}
 
-function diaDeLaSemana(numero) {
-    switch (numero) {
-        case 1: return "Lunes";
-        case 2: return "Martes";
-        case 3: return "Miércoles";
-        case 4: return "Jueves";
-        case 5: return "Viernes";
-        case 6: return "Sábado (fin de semana)";
-        case 7: return "Domingo (fin de semana)";
-        default: return "Día inválido";
-    }
-}
