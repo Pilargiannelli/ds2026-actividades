ef5b78143f1ac875385327fcd7349cd17a81a3e6
Pilar Giannelli <pilargiannelli@gmail.com>
2026-07-05 21:58:01 -0300

    Actividad 9

--- ejercicios/c10-formularios-zod/src/components/Layout/Layout.tsx
+++ ejercicios/c10-formularios-zod/src/components/Layout/Layout.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 export default function Layout({ children }: Props) {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow-1">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
